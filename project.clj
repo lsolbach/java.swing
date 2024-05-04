43e5b0e888832e73ea5e59490bcbe3148a215cd3
Ludger Solbach <soulman@soulspace.org>
2024-05-04 23:46:09 +0200

    released version 0.8.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.soulspace.clj/clj.swing "0.8.4-SNAPSHOT"
+(defproject org.soulspace.clj/clj.swing "0.8.4"
   :description "clj.swing is a library for building Java Swing user interfaces in Clojure"
   :url "https://github.com/soulspace-org/clj.swing"
   :license {:name "Eclipse Public License"
